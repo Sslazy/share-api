1294bc7bbad2478b32f230bad33bc556938ae1d0
zxy <1185790368@qq.com>
2023-10-07 15:42:12 +0800

    集成校验框架 validation

--- share-common/src/main/java/top/zxy/share/common/handler/ControllerExceptionHandler.java
+++ share-common/src/main/java/top/zxy/share/common/handler/ControllerExceptionHandler.java
@@ -1,6 +1,7 @@
 package top.zxy.share.common.handler;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -28,6 +29,16 @@ public class ControllerExceptionHandler {
         log.error("业务异常:",e);
         commonResp.setSuccess(false);
         commonResp.setMessage(e.getE().getDesc());
+        return commonResp;
+    }
+
+    @ExceptionHandler(value = BindException.class)
+    @ResponseBody
+    public CommonResp<?> exceptionHandler(BindException e){
+        CommonResp<?> commonResp = new CommonResp<>();
+        log.error("校验异常:{}",e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
+        commonResp.setSuccess(false);
+        commonResp.setMessage(e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
         return commonResp;
     }
 }
--- share-user/src/main/java/top/zxy/share/user/controller/UserController.java
+++ share-user/src/main/java/top/zxy/share/user/controller/UserController.java
@@ -1,6 +1,7 @@
 package top.zxy.share.user.controller;
 
 import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 import top.zxy.share.common.resp.CommonResp;
 import top.zxy.share.user.domain.dto.LoginDTO;
@@ -22,7 +23,7 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public CommonResp<User> login(@RequestBody LoginDTO loginDTO){
+    public CommonResp<User> login(@Valid  @RequestBody LoginDTO loginDTO){
         User user = userService.login(loginDTO);
         CommonResp<User> commonResp = new CommonResp<>();
         commonResp.setData(user);
