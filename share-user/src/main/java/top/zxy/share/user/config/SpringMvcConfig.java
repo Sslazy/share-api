ff39a571a901d9975a866d9bc3c5ebc790088083
zxy <1185790368@qq.com>
2023-10-08 14:01:08 +0800

    增加日志拦截器，统一打印日志流水号

--- share-user/src/main/java/top/zxy/share/user/config/SpringMvcConfig.java
+++ share-user/src/main/java/top/zxy/share/user/config/SpringMvcConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import top.zxy.share.common.interceptor.LogInterceptor;
-@Configuration
+//@Configuration
 public class SpringMvcConfig implements WebMvcConfigurer {
     @Resource
     private LogInterceptor logInterceptor;
